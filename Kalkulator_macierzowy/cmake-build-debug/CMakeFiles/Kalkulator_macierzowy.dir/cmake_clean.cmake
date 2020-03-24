d1f90c6167dcc1056cb36f5a1d8cb773e15526eb
MiloszBlachowiak <61923477+MiloszBlachowiak@users.noreply.github.com>
2020-03-24 20:33:33 +0100

    Add files via upload

---
+++ Kalkulator_macierzowy/cmake-build-debug/CMakeFiles/Kalkulator_macierzowy.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Kalkulator_macierzowy.dir/main.cpp.obj"
+  "Kalkulator_macierzowy.exe"
+  "Kalkulator_macierzowy.exe.manifest"
+  "Kalkulator_macierzowy.pdb"
+  "libKalkulator_macierzowy.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Kalkulator_macierzowy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Kalkulator_macierzowy/cmake-build-debug/CMakeFiles/Kalkulator_macierzowy.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Kalkulator_macierzowy.
+# This may be replaced when dependencies are built.
