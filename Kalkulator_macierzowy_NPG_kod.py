90675eed52090516002271c02f045fd63ef46b1e
MiloszBlachowiak <61923477+MiloszBlachowiak@users.noreply.github.com>
2020-08-11 22:44:55 +0200

    Dodano funkcję wyboru operacji

--- Kalkulator_macierzowy_NPG_kod.py
+++ Kalkulator_macierzowy_NPG_kod.py
@@ -1,1 +1,57 @@
-#tu pisać kod programu
+import numpy as np
+
+#funkcje wykonujące działania - uzupełnić!!!
+def addition():
+    pass
+
+def subtraction():
+    pass
+
+def multiplication():
+    pass
+
+def exponentiation():
+    pass
+
+def jordanForm():
+    pass
+
+def inversion():
+    pass
+
+def clearMemory():
+    pass
+
+
+def seePrevious():
+    pass
+
+
+#funkcja wyboru operacji
+def chooseAction():
+    action = input( "Podaj działanie na macierzach, które chcesz wykonać "
+                   "\n(wpisz taką nazwę działania jaka znajduje się na liście dostępnych operacji):" )
+
+    def switch(action):
+
+        switcher = {
+            "dodawanie": addition(),
+            "odejmowanie": subtraction(),
+            "mnożenie": multiplication(),
+            "potęgowanie": exponentiation(),
+            "wyprowadzanie postaci Jordana": jordanForm(),
+            "odwracanie macierzy": inversion(),
+            "czyszczenie pamięci": clearMemory(),
+            "odczytanie poprzedniego działania": seePrevious(),
+        }
+        return switcher.get(action,"Wprowadzono niepoprawną nazwę działania!")
+
+    switch(action)
+
+
+
+print( "Witaj w programie 'Kalkulator macierzowy'.\n \nDostępne operacje: \n-dodawanie, \n-odejmowanie, \n-mnożenie, \n"
+      "-potęgowanie, \n-wyprowadzanie postaci Jordana, \n-odwracanie macierzy, \n-czyszczenie pamięci,"
+      " \n-odczytanie poprzedniego działania.\n" )
+
+chooseAction()
