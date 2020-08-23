cae8a837b869b3cdd7ba22fdc52e264abec5dced
Karipolska <61945155+Karipolska@users.noreply.github.com>
2020-08-23 21:45:50 +0200

    Update Kalkulator_macierzowy_NPG_kod.py

--- Kalkulator_macierzowy_NPG_kod.py
+++ Kalkulator_macierzowy_NPG_kod.py
@@ -29,6 +29,16 @@ def isSquare(matrix):
         print("Macierz nie jest kwadratowa, wykonanie działania jest niemożliwe.")
         return False
     
+#funkcja wyboru sposobu wprowadzenia macierzy
+def choiceMatrix():
+    c=int(input("Aby wprowadzic macierz z klawiatury wybierz 1, aby wczytac z pliku wybierz 2)
+    if c==1:
+        enterMatrix()
+    elif c==2:
+        loadFile()
+    else:
+        print("Nie wybrałeś poprawnego numeru operacji)
+        return False
     
 #funkcja wczytywania macierzy z pliku
 def loadFile():
