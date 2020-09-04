a4109b8bb0db802318dee3a54190b4bf9d2b2ee6
Karipolska <61945155+Karipolska@users.noreply.github.com>
2020-09-04 19:56:08 +0200

    Update Kalkulator_macierzowy_NPG_kod.py
    Funkcja kontynuująca działanie programu po jednej operacji

--- Kalkulator_macierzowy_NPG_kod.py
+++ Kalkulator_macierzowy_NPG_kod.py
@@ -184,7 +184,17 @@ def chooseAction():
 
 
     switch(action)
-
+    next()
+    
+def next():
+    w = input("Aby kontynuować wpisz 1, aby wyjść wpisz 2\n")
+    if w == '1':
+        chooseAction()
+    if w == '2':
+        sys.exit("Program został zakończony")
+    else:
+        print("Wybrałeś złą opcję")
+        next()
 
 
 print( "Witaj w programie 'Kalkulator macierzowy'.\n")
